62ea3e0e2e53dbc0975ffda44511668e38a0777e
deepulearner <deepulearner@gmail.com>
2022-03-03 15:40:32 +0530

    SampleStudy

---
+++ Test Suites/ts_1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts_1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a72c5f55-0462-469b-ad15-eb2cc2b756b4</testSuiteGuid>
+   <testCaseLink>
+      <guid>b19d73e3-1627-43a6-9039-006920455a83</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc_1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
